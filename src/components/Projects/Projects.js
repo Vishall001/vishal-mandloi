da18d2f850b8d9d8b03c436c86b45813426c4148
Vishall001 <vshmandloi@gmail.com>
2022-10-07 14:16:40 +0530

    project link updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,7 +17,7 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
@@ -26,11 +26,10 @@ function Projects() {
               work=" Made the Cart and a payment page"
               ghLink="https://github.com/Vishall001/Farfetch-Clone"
               demoLink="https://peaceful-stroopwafel-aba1de.netlify.app/"
-            
               tech="HTML, CSS, Javascript"
             />
           </Col>
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={booking}
               isBlog={false}
@@ -38,11 +37,10 @@ function Projects() {
               description="Booking.com is a Dutch online travel agency for lodging reservations & other travel products, and a subsidiary of Booking Holdings. The website has over 28 million listings. The site is available in 43 languages."
               work=" Made the Homepage"
               ghLink="https://github.com/Vishall001/booking"
-              demoLink="https://clever-churros-0ca8ea.netlify.app/"
+              demoLink="https://bookingcom-clone.netlify.app/"
               tech="React, Redux, ChakraUI, Axios"
             />
           </Col>
-         
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -51,13 +49,11 @@ function Projects() {
               title="Overstock"
               description="Overstock.com, Inc. is an American internet retailer selling primarily furniture headquartered in Midvale, Utah, near Salt Lake City. Controversial businessman Patrick M. Byrne founded Overstock.com in 1999."
               work=" Made the products pages"
-
               ghLink="https://github.com/Vishall001/Overstock-Clone"
               demoLink="https://reliable-tanuki-4f48a8.netlify.app/"
               tech="Javascript, HTML, CSS"
             />
           </Col>
-         
         </Row>
       </Container>
     </Container>
